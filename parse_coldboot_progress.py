7d0d309ad824ab5dd697c787d450085a2545b347
Chen Tingjie <tingjie.chen@intel.com>
2016-12-20 22:43:39 +0800

    cold boot stage profiling re-structure
    Signed-off-by: Chen Tingjie <tingjie.chen@intel.com>

--- parse_coldboot_progress.py
+++ parse_coldboot_progress.py
@@ -12,9 +12,10 @@ class p_node:
         self.seconds = int(timestamps[0])*3600 + int(timestamps[1])*60 + float(timestamps[2])
 
 class stage_node(p_node):
-    def __init__(self, name, filterkey):
+    def __init__(self, name, filterkey, rank):
         p_node.__init__(self, name)
         self.filterkey = filterkey
+        self.rank = rank
 
 class svc_node(p_node):
     def __init__(self, name):
@@ -34,19 +35,32 @@ class bootpgs_parser:
         self.initrc_parser = initrc_parser()
 
     def initStageFramework(self):
-        self.nodeList.append(stage_node("init_start", "init started"))
-        self.nodeList.append(stage_node("zygote_start", "START com.android.internal.os.ZygoteInit"))
-        self.nodeList.append(stage_node("preload_start", "boot_progress_preload_start"))
-        self.nodeList.append(stage_node("preload_end", "boot_progress_preload_end"))
-        self.nodeList.append(stage_node("system_run", "boot_progress_system_run"))
-        self.nodeList.append(stage_node("pms_system_scan_start", "boot_progress_pms_system_scan_start"))
-        self.nodeList.append(stage_node("pms_data_scan_start", "boot_progress_pms_data_scan_start"))
-        self.nodeList.append(stage_node("pms_scan_end", "boot_progress_pms_scan_end"))
-        self.nodeList.append(stage_node("pms_ready", "boot_progress_pms_ready"))
-        self.nodeList.append(stage_node("ams_ready", "boot_progress_ams_ready"))
-        self.nodeList.append(stage_node("enable_screen", "boot_progress_enable_screen"))
+        self.nodeList.append(stage_node("init_start", "init started", 1))
+        self.nodeList.append(stage_node("initializing_selinux", "Initializing SELinux enforcing took", 2))
+        self.nodeList.append(stage_node(".coldboot_done", "Waiting for /dev/.coldboot_done took", 2))
+        
+        self.nodeList.append(stage_node("load_default_prop", "Loading properties from /default.prop took", 2))
+        self.nodeList.append(stage_node("load_system_prop", "Loading properties from /system/build.prop took", 2))
+        self.nodeList.append(stage_node("load_vendor_prop", "Loading properties from /vendor/build.prop took", 2))
+        self.nodeList.append(stage_node("load_factory_prop", "Loading properties from /factory/factory.prop took", 2))
+        self.nodeList.append(stage_node("load_data_local_prop", "Loading properties from /data/local.prop took", 2))
+        
+        self.nodeList.append(stage_node("cache_mount", "check_fs(): unmount(/cache)", 2))
+        self.nodeList.append(stage_node("config_mount", "check_fs(): unmount(/config)", 2))
+        self.nodeList.append(stage_node("data_mount", "check_fs(): unmount(/data)", 2))
+
+        self.nodeList.append(stage_node("zygote_start", "START com.android.internal.os.ZygoteInit", 1))
+        self.nodeList.append(stage_node("preload_start", "boot_progress_preload_start", 1))
+        self.nodeList.append(stage_node("preload_end", "boot_progress_preload_end", 1))
+        self.nodeList.append(stage_node("system_run", "boot_progress_system_run", 1))
+        self.nodeList.append(stage_node("pms_system_scan_start", "boot_progress_pms_system_scan_start", 1))
+        self.nodeList.append(stage_node("pms_data_scan_start", "boot_progress_pms_data_scan_start", 1))
+        self.nodeList.append(stage_node("pms_scan_end", "boot_progress_pms_scan_end", 1))
+        self.nodeList.append(stage_node("pms_ready", "boot_progress_pms_ready", 1))
+        self.nodeList.append(stage_node("ams_ready", "boot_progress_ams_ready", 1))
+        self.nodeList.append(stage_node("enable_screen", "boot_progress_enable_screen", 1))
         #self.nodeList.append(stage_node("display_launcher", "Displayed com.google.android.googlequicksearchbox/com.google.android.launcher.GEL"))
-        self.nodeList.append(stage_node("bootanim_exit", "Service 'bootanim'"))
+        self.nodeList.append(stage_node("bootanim_exit", "Service 'bootanim'", 1))
 
     def getLogs(self):
         logDir = "logs/"
@@ -81,11 +95,16 @@ class bootpgs_parser:
         self.initrc_parser.parse_initrc(self.outputDir)
 
     def showResult(self):
-        print("... show boot_progress result ...")
+        out = open(self.outputDir + self.resultFile,'a')
+        print("\n... show boot_progress result ...")
+        out.write("\n... show boot_progress result ...\n")
         print("---------------------")
+        out.write("---------------------\n")
         for node in self.nodeList:
-            print(node.name + ",\t\t" + str(node.seconds))
-        self.initrc_parser.showResult()
+            print((node.rank-1)*'\t' + '%-20s,'%(node.name) + (5-node.rank)*'\t' + '%5s'%(str(node.seconds)))
+            out.write((node.rank-1)*'\t' + '%-20s,'%(node.name) + (5-node.rank)*'\t' + '%5s'%(str(node.seconds)) + '\n')
+        out.close()
+        self.initrc_parser.showResult(self.outputDir + self.resultFile)
 
 class initrc_parser:
     def __init__(self):
@@ -151,13 +170,21 @@ class initrc_parser:
             svcnode.time_set(timestamp)
             self.svcList.append(svcnode)
 
-    def showResult(self):
-        print("... show service_start result ...")
+    def showResult(self, resultFile):
+        out = open(resultFile,'a')
+        print("\n... show service_start result ...")
+        out.write("\n... show service_start result ...\n")
         print("-------------------")
+        out.write("-------------------\n")
         for node in self.svcList:
-            print(node.name + ",\t" + node.phase + ",\t" + str(node.seconds))
+            # print(node.name + ",\t" + node.phase + ",\t" + str(node.seconds))
+            print('%-30s,'%(node.name) + 3*'\t' + '%5s,'%(str(node.seconds)) + 2*'\t' + '%10s'%(node.phase))
+            out.write('%-30s,'%(node.name) + 3*'\t' + '%5s,'%(str(node.seconds)) + 2*'\t' + '%10s'%(node.phase) + '\n')
+        out.close()
 
 if __name__ == '__main__':
+    os.system("adb root")
+    time.sleep(2)
     parser = bootpgs_parser()
     parser.initStageFramework()
     parser.getLogs()
